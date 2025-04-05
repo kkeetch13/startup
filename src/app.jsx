00f4f669832e5975129a9ef75e720d60af9a3684
Kaden Keetch <kkeetch76@gmail.com>
2025-04-05 22:32:44 +0000

    added github link

--- src/app.jsx
+++ src/app.jsx
@@ -43,8 +43,8 @@ export default function App() {
   
         <footer className="bg-dark text-white-50">
           <div className="container-fluid">
-            <span className="text-reset">Author Name(s)</span>
-            <a className="text-reset" href="https://github.com/webprogramming260/simon-react">
+            <span className="text-reset">Kaden Keetch</span>
+            <a className="text-reset" href="https://github.com/kkeetch13/startup.git">
               Source
             </a>
           </div>
