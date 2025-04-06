51777c3fde5acc986b2c210905b77cecc6cd4ceb
Kaden Keetch <kadenkeetch@Kadens-MacBook-Pro.local>
2025-04-05 18:45:11 -0600

    fixed css styling with react

--- src/app.jsx
+++ src/app.jsx
@@ -8,52 +8,68 @@ import { Projects } from './projects/projects';
 import { About } from './about/about';
 
 export default function App() {
-    return (
-        <BrowserRouter>
+  return (
+    <BrowserRouter>
       <div className="body bg-dark text-light">
-        <header className="container-fluid">
-          <nav className="navbar fixed-top navbar-dark">
-            <div className="navbar-brand">
-              Kaden Keetch
-            </div>
-            <menu className="navbar-nav">
-              <li className="nav-item">
-              <NavLink className="nav-link" to="">Login</NavLink>
-              </li>
-              <li className="nav-item">
-              <NavLink className="nav-link" to="play">Play</NavLink>
-              </li>
-              <li className="nav-item">
-              <NavLink className="nav-link" to="projects">Projects</NavLink>
-              </li>
-              <li className="nav-item">
-              <NavLink className="nav-link" to="about">About</NavLink>
-              </li>
-            </menu>
-          </nav>
-        </header>
-  
-        <main><Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/play" element={<Play />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes></main>
-  
-        <footer className="bg-dark text-white-50">
+        <header className="py-3">
           <div className="container-fluid">
+            <div className="d-flex justify-content-between align-items-center">
+              <div className="navbar-brand h1 mb-0">Kaden Keetch</div>
+              <nav>
+                <ul className="nav">
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/">
+                      Home
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/play">
+                      Play
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/projects">
+                      Projects
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/about">
+                      About
+                    </NavLink>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/play" element={<Play />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+
+        <footer className="bg-dark text-white-50 py-3">
+          <div className="container-fluid text-center">
             <span className="text-reset">Kaden Keetch</span>
-            <a className="text-reset" href="https://github.com/kkeetch13/startup.git">
+            <a className="text-reset ms-2" href="https://github.com/kkeetch13/startup.git">
               Source
             </a>
           </div>
         </footer>
       </div>
-      </BrowserRouter>
-    );
-  }
+    </BrowserRouter>
+  );
+}
 
-  function NotFound() {
-    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
-  }
+function NotFound() {
+  return (
+    <main className="container-fluid bg-secondary text-center py-5">
+      404: Return to sender. Address unknown.
+    </main>
+  );
+}
