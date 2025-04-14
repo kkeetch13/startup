2cbe5dbd68753e53cff41fcea1bbcf11975418b3
Kaden Keetch <kadenkeetch@Kadens-MacBook-Pro.local>
2025-04-14 03:00:11 -0600

    fixed login authorization error

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // relative paths for production build
+  base: './',
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
